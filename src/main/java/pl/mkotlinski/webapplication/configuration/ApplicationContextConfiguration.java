0cf8645284ae17fcd0bfebe753fcbaf60f4e34e6
Michał Kotliński <michal.kotlinski@comarch.pl>
2017-07-06 14:42:30 +0200

    Project initialization.

---
+++ src/main/java/pl/mkotlinski/webapplication/configuration/ApplicationContextConfiguration.java
@@ -0,0 +1,41 @@
+package pl.mkotlinski.webapplication.configuration;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class ApplicationContextConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware{
+	
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;		
+	}
+	
+	//Messages.properties
+	@Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        source.setUseCodeAsDefaultMessage(true);
+        return source;
+    }
+	
+	//ViewResolver
+	@Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
